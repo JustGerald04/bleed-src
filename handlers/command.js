73c10ac1533b1824c92bf995bbd3d86d623c05bb
JustGerald04 <gamingwithmahboiuriah@gmail.com>
2026-06-12 13:57:39 -0400

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -6,23 +6,27 @@ let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
+    // We changed "./commands/" to "./" to read the root folders instead
+    // We filter out system folders that aren't command categories
+    const categories = ["fun", "information", "lastfm", "moderation", "owner", "utility"];
 
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    categories.forEach(dir => {
+        const commands = readdirSync(`./${dir}/`).filter(file => file.endsWith(".js"));
 
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+            let pull = require(`../${dir}/${file}`);
 
             if (pull.name) {
                 client.commands.set(pull.name, pull);
                 table.addRow(file, '✅');
             } else {
-                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+                table.addRow(file, `❌ -> missing a help.name, or help.name is not a string.`);
                 continue;
             }
+
             if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
     });
 
-    //console.log(table.toString());
+    console.log(table.toString());
 }
